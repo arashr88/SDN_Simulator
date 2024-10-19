26ee7fd27108860d83bf74b2b2c7c8fc0aa2937a
Kwadwo A Bempah <kwadwoasiedu@bempah.com>
2024-10-19 06:49:18 -0400

    Fix scroll sensitivity on pick-able item
    Scrolling on plot items would trigger the Node dialog which was annoying.
    Now fixed to register a single mouse left-click.

--- gui_scripts/gui_helpers/topology_helpers.py
+++ gui_scripts/gui_helpers/topology_helpers.py
@@ -38,7 +38,7 @@ class TopologyCanvas(FigureCanvas):
         self.axes = fig.add_subplot(111)
         super().__init__(fig)  # pylint: disable=super-with-arguments
         self.setParent(parent)
-
+        self.scatter = None
         self.G = None  # pylint: disable=invalid-name
 
     def plot(self, graph, pos):  # pylint: disable=invalid-name
@@ -67,21 +67,24 @@ class TopologyCanvas(FigureCanvas):
 
         :param scatter: The scatter object of the topology.
         """
+        self.scatter = scatter
         scatter.set_picker(True)
-        self.mpl_connect('pick_event', self.on_pick)
+        self.mpl_connect('button_press_event', self.on_pick)
 
     def on_pick(self, event):
         """
-        Handles event to display node information on a click.
+        Handles event to display node information on a click. If user left-clicked near
+        a pick-able artifact on the plot, show NodeDialog.
 
         :param event: The event object.
         """
-        ind = event.ind[0]
-        node = list(self.G.nodes())[ind]
-        info = "Additional Info: ..."  # Replace with actual node information
-        dialog = NodeInfoDialog(node, info, self.parent())
-        dialog.setWindowModality(qtc.Qt.ApplicationModal)
-        dialog.setWindowFlag(qtc.Qt.WindowStaysOnTopHint)
-        dialog.raise_()
-        dialog.activateWindow()
-        dialog.exec_()
+        if event.button == 1:
+            contains, index = self.scatter.contains(event)
+            if contains:
+                ind = index['ind'][0]
+                node = list(self.G.nodes())[ind]
+                info = "Additional Info: ..."  # Replace with actual node information
+                dialog = NodeInfoDialog(node, info, self.parent())
+                dialog.setWindowModality(qtc.Qt.ApplicationModal)
+                dialog.setWindowFlag(qtc.Qt.WindowStaysOnTopHint)
+                dialog.show()
