34189d65038394088992a4b8bb7d5d15064680b1
unknown <rezaee.arash88@gmail.com>
2025-01-01 11:53:27 -0500

    add Spain topology

--- data_scripts/structure_data.py
+++ data_scripts/structure_data.py
@@ -71,6 +71,11 @@ def create_network(net_name: str, base_fp: str = None, const_weight: bool = Fals
         if not is_only_core_node:
             core_nodes_fp = os.path.join(base_fp, 'USB6014_core_nodes.txt')
             core_nodes_list = assign_core_nodes(core_nodes_fp=core_nodes_fp)
+    elif net_name == 'Spainbackbone30':
+        network_fp = os.path.join(base_fp, 'SPNB3014.txt')
+        if not is_only_core_node:
+            core_nodes_fp = os.path.join(base_fp, 'SPNB3014_core_nodes.txt')
+            core_nodes_list = assign_core_nodes(core_nodes_fp=core_nodes_fp)
     else:
         raise NotImplementedError(f"Unknown network name. Expected USNet, NSFNet, or Pan-European. Got: {net_name}")
 
